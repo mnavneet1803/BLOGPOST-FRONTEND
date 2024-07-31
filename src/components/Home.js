dd6ce6e374a2ab644aa681bc5078851af97901cf
mnavneet1803 <102963603+mnavneet1803@users.noreply.github.com>
2024-07-31 18:51:14 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Axios from "axios";
-
 import { useEffect, useState } from 'react';
 import Post from './Post'
 
@@ -16,13 +15,22 @@ function Home() {
     getData()
   }, []);
 
+
+
+
   return (
     <>
+
+        <div className='container d-flex flex-wrap'>
+
       {
         data1 && data1.AllPosts.length > 0 && data1.AllPosts.map((item, index) => (
+
+
           <Post key={index} id={item.id} title={item.title} content={item.content} src={item.imageUrl} createdAt={item.createdAt} />
         ))
       }
+      </div>
     </>
   );
 }
